9f585f6d2d435523e18c83e426b390b8924de65e
Carlos G. Gavidia <cgavidiac@gmail.com>
2017-02-23 18:06:57 +0000

    Domain classes

--- src/main/java/hashcode/videos/VideoCaching.java
+++ src/main/java/hashcode/videos/VideoCaching.java
@@ -1,5 +1,17 @@
 package hashcode.videos;
 
-public class VideoCaching {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import hashcode.mainevent.IProblem;
+
+public class VideoCaching implements IProblem {
+
+	@Override
+	public void readFile(File f) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+
+	}
 
 }
